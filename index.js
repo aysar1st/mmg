17491f7de0f8d98ac2e0cf478be8f8b9b0a62742
aysar1st <aysr9055@gmail.com>
2026-01-17 04:16:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
